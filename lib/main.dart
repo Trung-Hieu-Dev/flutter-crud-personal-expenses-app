8e85b25140bbf6cd347989f6f4dd3ef64781fc44
Trung Hieu <hieu@ltv.dev>
2023-04-07 05:44:14 +0700

    Column Alignment

--- lib/main.dart
+++ lib/main.dart
@@ -19,8 +19,23 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
-      body: Center(
-        child: Text('Widget Playground!'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              elevation: 5,
+              child: Text('Chart'),
+            ),
+          ),
+          Card(
+            color: Colors.red,
+            child: Text('List'),
+          ),
+        ],
       ),
     );
   }
